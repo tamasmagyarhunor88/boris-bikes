25b4bca056ad256656df88ef0fdee5c85c5b9db9
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-01-26 14:24:45 +0000

    tested for releasing and working bikes, methods pass

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,15 @@
 require 'docking_station'
 
 RSpec.describe DockingStation do # this is test for creating an instance of specified class
-  describe '#release_bike' do # shorthand-longhand for this testing syntax
+  # describe '#release_bike' do # shorthand-longhand for this testing syntax
     it { is_expected.to respond_to(:release_bike) }
-  end # https://relishapp.com/rspec/rspec-core/v/3-2/docs/subject/one-liner-syntax
+  # end # https://relishapp.com/rspec/rspec-core/v/3-2/docs/subject/one-liner-syntax
+
+    it 'releases bike' do
+      expect(subject.release_bike).to be_instance_of(Bike)
+    end
+
+    it 'expects the bike.working? to return true' do
+      expect(subject.release_bike).to be_working
+    end
 end
